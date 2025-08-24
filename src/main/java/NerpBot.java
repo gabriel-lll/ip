330a02fde832378d86e236f02f56b10ba5cfff05
gabriel-lll <gabrielsim404@gmail.com>
2025-08-24 14:34:31 +0800

    Echos commands and exits on bye

--- src/main/java/NerpBot.java
+++ src/main/java/NerpBot.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class NerpBot {
+    private static void printDivider() {
+      System.out.println("______________________________");
+    }
     public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      String command;
+
+      printDivider();
       System.out.println("Hello! I'm NerpBot");
       System.out.println("What can I do for you?");
-      System.out.println("Bye. Hope to see you again soon!");
+      printDivider();
+
+      while (true) {
+        command = scanner.nextLine();
+        if (command.equals("bye")) {
+          printDivider();
+          System.out.println("Bye. Hope to see you again soon!");
+          printDivider();
+          break;
+        } else {
+          printDivider();
+          System.out.println(command);
+          printDivider();
+        }
+      }
     }
 }
