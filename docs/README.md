584c35be47d5d4321d8a101e65a23a9c9379eb23
gabriel-lll <gabrielsim404@gmail.com>
2025-08-24 14:12:18 +0800

    Renamed bot and added greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# NerpBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/NerpBot.java
@@ -0,0 +1,7 @@
+public class NerpBot {
+    public static void main(String[] args) {
+      System.out.println("Hello! I'm NerpBot");
+      System.out.println("What can I do for you?");
+      System.out.println("Bye. Hope to see you again soon!");
+    }
+}
